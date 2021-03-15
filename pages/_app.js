1f082a6b4a33f62848e677ad9ad37ea4c3a49293
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-03-15 20:17:28 -0300

    Limpieza

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
 // import '../styles/globals.css'
-import '../styles/classless.css'
-
+import "../styles/classless.css";
 
 const App = ({ Component, pageProps }) => (
   <MDXProvider>
     <Component {...pageProps} />
-  </ MDXProvider>
+  </MDXProvider>
 );
 
 export default App;
